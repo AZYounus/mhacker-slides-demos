e075f10a6343215faab90355c3fe26ffec57217f
AZYounus <azyounus@umich.edu>
2025-11-06 16:43:28 -0500

    made grid for github cards

--- my-react-app/src/components/GithubCard.jsx
+++ my-react-app/src/components/GithubCard.jsx
@@ -1,19 +1,21 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
-function GithubCard({ image_url }) {
+function GithubCard({ image_url, project_title, className, style }) {
   return (
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title><img src={image_url} />Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
+    <div className={className} style={style}>
+      <Card style={{ width: '24rem' }}>
+        <Card.Img variant="top" src={image_url} style={{objectFit: 'fill'}}/>
+        <Card.Body>
+          <Card.Title>{project_title}</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+    </div>
   );
 }
 
--- my-react-app/src/pages/Projects.jsx
+++ my-react-app/src/pages/Projects.jsx
@@ -1,37 +1,47 @@
 import { useEffect, useState } from 'react'
 import GithubCard from '../components/GithubCard'
+import './Projects.css'
 
 function Projects() {
     const [reposArray, setReposArray] = useState([]);
     const [avatarURL, setAvatarURL] = useState();
+    const [createdAtArray, setCreatedAtArray] = useState([]);
 
-    // runs on initial page render
-    useEffect(() => {
-        fetch("https://api.github.com/users/AZYounus")
-            .then((res) => res.json()
-            ).then((result) => {
+    function fetchUserPage(user) {
+        fetch(`https://api.github.com/users/${user}`)
+            .then((res) => res.json())
+            .then((result) => {
                 console.log(result)
                 setAvatarURL(result.avatar_url)
             }, (error) => {
                 console.log(error)
             }
             )
-        fetch("https://api.github.com/users/AZYounus/repos")
-            .then((res) => res.json()
-            ).then((result) => {
-                console.log(result)
-                result.map((elem) => {
-                    reposArray.push(elem)
-                })
+    }
+
+    function fetchUserRepos(user) {
+        fetch(`https://api.github.com/users/${user}/repos`)
+            .then((res) => res.json())
+            .then((result) => {
+                setReposArray(result)
                 console.log(reposArray)
             }, (error) => {
                 console.log(error)
             }
             )
+    }
+    // runs on initial page render
+    useEffect(() => {
+        fetchUserPage("AZYounus")
+        fetchUserRepos("AZYounus")
     }, [])
     return (
         <>
-        <GithubCard image_url={avatarURL}/>
+        <div className="gitHubCards">
+            {reposArray.map((repo, index) => (
+                <GithubCard className={`GitHubCard${index}`} image_url={avatarURL} project_title={repo.name} key={index} style={{gridArea: `GitHubCard${index}`}} />
+            ))}
+        </div>
         </>
     )
 }
